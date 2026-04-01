db63eb82e711a92f4a8cb25fc8714eb0dbbb567b
Olusoga-Peters Oladeji <oladejipeters@gmail.com>
2026-04-01 20:33:25 +0100

    correcting error response

--- goodweather.js
+++ goodweather.js
@@ -64,6 +64,7 @@ async function submitHandler(event) {
       temperature.textContent = "";
       humidity.textContent = "";
       windSpeed.textContent = "";
+      weatherCondition.textContent = "";
       return;
     }
 
